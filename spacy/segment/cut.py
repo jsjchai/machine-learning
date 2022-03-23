a88a3bcd9fe5b44df2406894ff1993a6a087879b
jsjchai <jsjchai@gmail.com>
2022-03-23 09:40:10 +0800

    spacy训练模型

---
+++ spacy/segment/cut.py
@@ -0,0 +1,23 @@
+import spacy_pkuseg
+from spacy.lang.zh import Chinese
+
+# pip install https://github.com/lancopku/pkuseg-python/archive/master.zip
+import pkuseg
+
+# jieba/pkuseg
+cfg = {"segmenter": "pkuseg"}
+nlp = Chinese.from_config({"nlp": {"tokenizer": cfg}})
+
+# pkuseg_model="spacy_ontonotes" pkuseg_model="news" medicine tourism web mixed
+nlp.tokenizer.initialize(pkuseg_model="web")
+
+nlp.tokenizer.pkuseg_update_user_dict(["杭州大厦"])
+
+print(nlp.tokenizer.pkuseg_seg)
+#spacy_pkuseg.pkuseg(postag=True)
+
+text = "我要和她去杭州大厦吃饭"
+doc = nlp(text)
+
+for token in doc:
+    print(token.text, token.dep_, token.pos_, token.tag_)
--- spacy/span-train/scripts/evaluate.py
+++ spacy/span-train/scripts/evaluate.py
@@ -1,7 +1,10 @@
 import spacy
+from spacy import displacy
 
 nlp = spacy.load('../output/model-best')
 
-doc = nlp("我要买iPhone X")
+doc = nlp("我要买iPhone X和mac")
 for token in doc.ents:
     print(token)
+
+displacy.serve(doc, style="ent", port=9797)
